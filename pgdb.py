a7dd4abb9a717b77aa7a828d269a407675e8b271
eurico-dias <eurico-dias>
2020-04-14 11:18:34 +0100

    Refactoring of database adapter solution

---
+++ pgdb.py
@@ -0,0 +1,92 @@
+import configparser
+import psycopg2
+
+class PGDB(object):
+    '''Relational database class definition.'''
+
+    def __init__(self):
+        config = configparser.ConfigParser()
+        config.read('options.conf')
+
+        self.url = config['postgresql']['URL']
+        self.port = config['postgresql']['PORT']
+        self.db = config['postgresql']['DB']
+        self.user = config['postgresql']['USER']
+        self._pw = config['postgresql']['PW']
+
+    def __str__(self):
+        return f"PGDB -> (url={self.url},port={self.port},user={self.user},db={self.db})"
+
+    def __repr__(self):
+        return self.__str__()
+
+    def getRooms(self):
+        db_con = psycopg2.connect(host=self.url, port=self.port, user=self.user, password=self._pw, dbname=self.db)
+        cursor = db_con.cursor()
+        cursor.execute("SELECT ID FROM Espaco")
+        result = cursor.fetchall()
+        db_con.close()
+        return result
+
+    def getRoom(self, roomid):
+        db_con = psycopg2.connect(host=self.url, port=self.port, user=self.user, password=self._pw, dbname=self.db)
+        cursor = db_con.cursor()
+        cursor.execute("SELECT Nome,Descricao FROM Espaco WHERE ID = %s", (roomid))
+        result = cursor.fetchone()
+        result = {"name" : result[0], "description" : result[1]}
+        db_con.close()
+        return result
+
+    def createRoom(self, roomid, roomdata, sensors):
+        db_con = psycopg2.connect(host=self.url, port=self.port, user=self.user, password=self._pw, dbname=self.db)
+        cursor = db_con.cursor()
+        cursor.execute("INSERT INTO Espaco VALUES (%s, %s, %s);", (roomid, roomdata["name"], roomdata["description"]))
+        for s in sensors:
+            cursor.execute("UPDATE Sensor SET ID_Espaco = %s WHERE ID = %s;", (roomid, s))
+        db_con.close()
+
+
+
+    def getSensorsFromRoom(self, roomid):
+        db_con = psycopg2.connect(host=self.url, port=self.port, user=self.user, password=self._pw, dbname=self.db)
+        cursor = db_con.cursor()
+        cursor.execute("SELECT Sensor.ID FROM Espaco JOIN Sensor ON Espaco.ID=Sensor.ID_Espaco WHERE Espaco.ID = %s", (roomid))
+        result = cursor.fetchall()
+        db_con.close()
+        return result
+
+    def updateSensorsFromRoom(self, roomid, changes):
+        db_con = psycopg2.connect(host=self.url, port=self.port, user=self.user, password=self._pw, dbname=self.db)
+        cursor = db_con.cursor()
+        for s in changes["add"]:
+            cursor.execute("UPDATE Sensor SET ID_Espaco = %s WHERE ID = %s;", (roomid, s))
+
+        for s in changes["remove"]:
+            cursor.execute("UPDATE Sensor SET ID_Espaco = Null WHERE ID = %s;", (roomid, s))
+
+        db_con.close()
+
+    def getSensor(self, sensorid):
+        db_con = psycopg2.connect(host=self.url, port=self.port, user=self.user, password=self._pw, dbname=self.db)
+        cursor = db_con.cursor()
+        cursor.execute("SELECT Sensor.Descricao,Nome_TipoSensor,Simbolo,Nome,Espaco.Descricao FROM Sensor JOIN Espaco ON Sensor.ID_Espaco=Espaco.ID WHERE Sensor.ID = %s",  (sensorid))
+        result = cursor.fetchone()
+        result = {"description" : result[0],
+                "data" : { "type": result[1], "unit_symbol": result[2]},
+                "room" : { "name": result[3], "description": result[4]}
+                }
+        db_con.close()
+        return result
+
+    def createSensor(self, sensorid, sensordata):
+        db_con = psycopg2.connect(host=self.url, port=self.port, user=self.user, password=self._pw, dbname=self.db)
+        cursor = db_con.cursor()
+        cursor.execute("INSERT INTO Sensor VALUES (%s, %s, %s, %s, NULL);",(sensorid, sensordata["description"], sensordata["data"]["type"], sensordata["data"]["unit_symbol"]))
+        db_con.close()
+
+
+    def updateSensor(self, sensorid, sensordata):
+        db_con = psycopg2.connect(host=self.url, port=self.port, user=self.user, password=self._pw, dbname=self.db)
+        cursor = db_con.cursor()
+        cursor.execute("UPDATE Sensor SET ID_Espaco = %s WHERE ID = %s;", (sensordata["room_id"], sensorid))
+        db_con.close()
--- wsgi.py
+++ wsgi.py
@@ -1,29 +1,15 @@
+import configparser
 import logging
 
-import db
-import configparser
 from werkzeug.exceptions import NotFound
 from werkzeug.middleware.dispatcher import DispatcherMiddleware
 
 from api import app, csrf, paranoid, APP_BASE_ENDPOINT, VERSION
 
-def no_app(environ, start_response):
-    return NotFound()(environ, start_response)
+def app_not_found(env, resp):
+    return NotFound()(env, resp)
 
 config = configparser.ConfigParser()
-config.read('options.conf')
-
-IURL = config['influxdb']['URL']
-IPORT = config['influxdb']['PORT']
-IDB = config['influxdb']['DB']
-IUSER = config['influxdb']['USER']
-IPW = config['influxdb']['PW']
-
-PGURL = config['postgresql']['URL']
-PGPORT = config['postgresql']['PORT']
-PGDB = config['postgresql']['DB']
-PGUSER = config['postgresql']['USER']   
-PGPW = config['postgresql']['PW']
 
 config.read(".appconfig")
 ck = config['info']['consumer_key']
@@ -31,12 +17,11 @@ cs = config['info']['consumer_secret']
 
 app.config['SECRET_KEY'] = config['info']['app_key']
 app.config['APPLICATION_ROOT'] = f"/{APP_BASE_ENDPOINT}/{VERSION}"
-db.init_dbs(PGURL, PGPORT, PGDB, PGUSER, PGPW, IURL, IUSER, IPW, IPORT, IDB)
 
 csrf.init_app(app)
 paranoid.init_app(app)
 
-app.wsgi_app = DispatcherMiddleware(no_app, {f"/{APP_BASE_ENDPOINT}/{VERSION}": app.wsgi_app})
+app.wsgi_app = DispatcherMiddleware(app_not_found, {f"/{APP_BASE_ENDPOINT}/{VERSION}": app.wsgi_app})
 
 gunicorn_logger = logging.getLogger('gunicorn.access')
 app.logger.handlers = gunicorn_logger.handlers
