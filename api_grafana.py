beb5b14d801ef2d3017e3ceec5e0fb40c0f36006
Ricardo Carvalho <rapc99@ua.pt>
2020-04-14 12:46:53 +0100

    Query endpoint (WIP)

--- api_grafana.py
+++ api_grafana.py
@@ -3,13 +3,20 @@ Flask REST API endpoints for Grafana support
 """
 
 import db_queries
+import db
+import _strptime
 import uuid
 
+from calendar import timegm
+from datetime import datetime
 from flask import Blueprint, jsonify, request, Response
 from flask_cors import CORS
 
 grafana = Blueprint('grafana', __name__,url_prefix='/grafana')
 CORS(grafana)
+
+def convert_to_time_ms(timestamp):
+    return 1000 * timegm(datetime.strptime(timestamp, '%Y-%m-%dT%H:%M:%S.%fZ').timetuple())
 
 @grafana.route('/', methods=['GET', 'POST', 'OPTIONS'])
 def graf_root():
@@ -24,13 +31,33 @@ def graf_search():
         sensors= db_queries.getSensorsFromRoom(r)
         #sensors= [(uuid.uuid4()),(uuid.uuid4()),(uuid.uuid4())]
         for s in sensors:
-            res.append("Room"+r+"_Sensor"+str(s))
+            res.append('Room'+r+'_'+str(s))
     return jsonify(res)
 
 @grafana.route('/query', methods=['POST'])
 def graf_query():
-    return jsonify([])
+    req= request.get_json()
+    targets= []
+    for t in req['targets']:
+        if 'target' in t.keys():
+            targets.append((t['target']).split('_')[1])
+    if targets == []:
+        return jsonify([])
+    time_st= convert_to_time_ms(req['range']['from'])
+    time_end= convert_to_time_ms(req['range']['to'])
+    res= []
+    for t in targets:
+        query= db.query_interval(uuid.UUID(t),time_st,time_end)
+        if not query=={}:
+            result= query['values']
+            datapoints= []
+            for r in result:
+                datapoints.append([r['value'],convert_to_time_ms(r['time'])])
+            res.append({"target":t,"datapoints":datapoints})    
+    return jsonify(res)
 
 @grafana.route('/annotations', methods=['POST'])
 def graf_annotations():
     return jsonify([])    
+
+print(graf_query())
