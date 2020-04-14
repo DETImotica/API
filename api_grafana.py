cac54215ee9d610f5a3e939814cb83d0922349b4
eurico-dias <eurico-dias>
2020-04-14 11:28:37 +0100

    Changed Grafana endpoint db modules usage

--- api_grafana.py
+++ api_grafana.py
@@ -2,14 +2,19 @@
 Flask REST API endpoints for Grafana support
 """
 
-import db_queries
 import uuid
 
 from flask import Blueprint, jsonify, request, Response
 from flask_cors import CORS
 
+from pgdb import PGDB
+from datadb import DataDB
+
 grafana = Blueprint('grafana', __name__,url_prefix='/grafana')
 CORS(grafana)
+
+pgdb = PGDB()
+influxdb = DataDB()
 
 @grafana.route('/', methods=['GET', 'POST', 'OPTIONS'])
 def graf_root():
@@ -17,11 +22,11 @@ def graf_root():
 
 @grafana.route('/search', methods=['POST'])
 def graf_search():
-    rooms= db_queries.getRooms()
+    rooms= pgdb.getRooms()
     #rooms= [('12'),('23'),('34')]
     res= []
     for r in rooms:
-        sensors= db_queries.getSensorsFromRoom(r)
+        sensors= pgdb.getSensorsFromRoom(r)
         #sensors= [(uuid.uuid4()),(uuid.uuid4()),(uuid.uuid4())]
         for s in sensors:
             res.append("Room"+r+"_Sensor"+str(s))
