94f2b829aac991fda5d5b320b14516f24d02ecef
Ricardo Carvalho <rapc99@ua.pt>
2020-05-13 21:58:42 +0100

    Notifications endpoint corrections

--- api_grafana.py
+++ api_grafana.py
@@ -44,6 +44,8 @@ def graf_search():
 
 @grafana.route('/query', methods=['POST'])
 def graf_query():
+    if not request.json:
+        return Response(json.dumps({"error_description": "Empty JSON or empty body."}), status=400,mimetype='application/json')
     req= request.json
     targets= []
     for t in req['targets']:
